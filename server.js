8c341ab7f28a6ba4e171438d4cf2c5257ccb9701
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-07-03 19:22:32 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+//const host = process.env.HOST || "localhost"
+app.use('/', require('./routers'));
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
