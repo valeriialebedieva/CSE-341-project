64bc0abe848a91e757e677f8aa5f49d2a83c9381
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-07-05 11:46:42 +0200

    added w2 task

--- routers/contacts.js
+++ routers/contacts.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-router.use(express.json());
-
 const userController = require('../controllers/contacts');
 
 // GET 
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const mongodb = require('./data/db');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
 
 app.use('/', require('./routers'));
 
